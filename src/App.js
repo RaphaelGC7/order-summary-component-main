269ee0ee55bd3b2d4e718188063e33946adddf72
Rafael <raphaelgc@outlook.com>
2021-09-10 13:56:37 -0500

    Added the top cover and tittle to the TopAndContent component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import './App.css';
+import TopAndContent from './components/TopAndContent';
 
 function App() {
   return (
     <div className="App">
+      <TopAndContent/>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
---
+++ src/components/TopAndContent.js
@@ -0,0 +1,13 @@
+import './TopAndContent.css'
+
+function TopAndContent(){
+    return (
+        <div id="TopAndContent">
+            <img src="illustration-hero.svg" alt="People having fun listening to music"/>
+            <h1>Order Summary</h1>
+        </div>
+    );
+
+}
+
+export default TopAndContent;
