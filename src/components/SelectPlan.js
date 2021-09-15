86fd74704a65deeabef30554b90f1efd32e6b33a
Rafael Gama <36249407+RaphaelGC7@users.noreply.github.com>
2021-09-15 00:10:15 -0500

    Added select plan component

---
+++ src/components/SelectPlan.js
@@ -0,0 +1,15 @@
+function SelectPlan (props){
+    return (
+        <div className="SelectPlan">
+            <div className="SelectPlan-icon">
+            <img src="icon-music.svg" alt="Select Plan icon music" className="SelectPlan-img"/>
+            </div>
+            <div className="SelectPlan-content">
+                <p>Annual Plan</p>
+                <p>$59.99/year</p>
+            </div>
+        </div>
+    );
+}
+
+export default SelectPlan;
--- src/components/TopAndContent.js
+++ src/components/TopAndContent.js
@@ -1,4 +1,6 @@
 import './TopAndContent.css'
+import React from 'react';
+import SelectPlan from './SelectPlan';
 
 function TopAndContent(){
     return (
@@ -6,6 +8,7 @@ function TopAndContent(){
             <img id="HeroImage" src="illustration-hero.svg" alt="People having fun listening to music"/>
             <h1>Order Summary</h1>
             <p>You can now listen to millions of songs, audiobooks, and podcasts on any device anywhere you like!</p>
+            <SelectPlan/>
         </div>
     );
 
